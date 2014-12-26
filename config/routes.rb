88ed5c3fbf39d87cef4f1d45784f0dd7eb403528
An.nA <anna.git3@gmail.com>
2014-12-25 23:26:04 -0800

    contact form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # any request to the application root will now be directed to the VisitorsController new action
+  
+  
+  resources :contacts, only: [:new, :create]
   root to: 'visitors#new'
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
