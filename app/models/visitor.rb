a67e0c3f885ba6aebdeec95962d80f8cbd3fd4d7
An.nA <anna.git3@gmail.com>
2014-12-30 02:58:08 -0800

    fix list id bug

--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -6,15 +6,15 @@ class Visitor
 	validates_format_of :email, :with => /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i
 
 	def subscribe
-		newchimp = Gibbon::API.new(Rails.application.secrets.mailchimp_api_key)
-		resultare = newchimp.lists.subscribe({
+		mailchimp = Gibbon::API.new(Rails.application.secrets.mailchimp_api_key)
+		result = mailchimp.lists.subscribe({
 			:id => Rails.application.secrets.mailchimp_list_id,
 			:email => {:email => self.email},
-			:double_option => false,
+			:double_optin => false,
 			:update_existing => true,
 			:send_welcome => true
 			})
-		Rails.logger.info("Subscribed #{self.email} to MailChimp") if resultare
+		Rails.logger.info("Subscribed #{self.email} to MailChimp") if result
 	end
 
 end
